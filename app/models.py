e2f68f1c23b41ce2b985d968658bd05a49975dfd
Bezariusg <Bezariusg@users.noreply.github.com>
2020-11-17 01:03:20 -0300

    galeria casi completa
    Co-Authored-By: null <zoul007@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Liga(models.Model):
+    nombre = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.nombre
+
+class Server(models.Model):
+    nombre = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.nombre
+
+class Aspirante(models.Model):
+    nombre = models.CharField(max_length = 15)
+    correo = models.CharField(max_length = 30)
+    nombre_invocador = models.CharField(max_length = 15)
+    servidor = models.ForeignKey(Server, on_delete =models.PROTECT)
+    Liga = models.ForeignKey(Liga, on_delete =models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
