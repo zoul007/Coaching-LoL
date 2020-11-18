670cbef7970d7cef6ab751f73135f494f38f7bc5
Felipe Herrera <zoulpro@gmail.com>
2020-11-17 22:07:02 -0300

    Imagenes Crud

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Aspirante, Liga, Server
+from .models import Aspirante, Liga, Server, imagenes_galeria
 # Register your models here.
 
 admin.site.register(Aspirante)
 admin.site.register(Liga)
 admin.site.register(Server)
+admin.site.register(imagenes_galeria)
