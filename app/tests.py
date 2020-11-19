63f9258c87c511725abbdfb3f8d7fd512155d444
Bezariusg <Bezariusg@users.noreply.github.com>
2020-11-19 19:32:14 -0300

    .

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from unittest
+from models import *
 
 # Create your tests here.
+
+
+class testServer (unittest.TestCase):
+
+    def test_nombre_server(self):
+        # Cuando se trata de cargar un archivo que no es formato de imagen
+        self.assertAlmostEqual(Server("AMéricA"), "América" )
+
+if __name__ == "__main__":
+    unittest.main()
