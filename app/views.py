3aa0c3b6578760076a92f520333d73fc0fa138c1
Felipe Herrera <zoulpro@gmail.com>
2020-11-19 20:29:43 -0300

    Registro de usuario

--- app/views.py
+++ app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import *
 from django.forms import modelformset_factory
 
-from .forms import AspiranteForm
+from .forms import AspiranteForm, CustUserCreationForm
 from django.contrib import messages
+
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -42,3 +45,20 @@ def image_gallery(request):
     images = Image.objects.all()
     print(images)
     return render(request, 'app/image_gallery.html', {'images': images})
+
+def registro(request):
+    data = {
+        'form': CustUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username = formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "Ingreso al sistema Exitoso")
+            return redirect(to="home")
+        data["form"] = formulario
+
+    return render(request, 'registration/registro.html',data)
