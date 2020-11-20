c50e250bc90d56e014b3f8ca48caf88621e5e051
Bezariusg <Bezariusg@users.noreply.github.com>
2020-11-20 00:03:22 -0300

    envio

--- app/urls.py
+++ app/urls.py
@@ -17,20 +17,8 @@ urlpatterns = [
     path('image_gallery/', views.image_gallery, name='image_gallery'),
     path('registro/', registro , name="registro"),
     path('reset-password/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-<<<<<<< HEAD
-
-    #path('reset_password/',
-    #PasswordResetView.as_view(template_name="registration/password_reset.html"),
-    #name="reset_password"),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name='password_reset'),
-
-    path('reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/',
-    PasswordResetConfirmView, name='password_reset_confirm'),
-=======
     path('reset-password/', PasswordResetView.as_view(), name='password_reset'),
     path('reset-password/confirm/<uidb64>/<token>/', PasswordResetConfirmView, name='password_reset_confirm'),
     path('reset-password-complete/', PasswordResetCompleteView, name='password_reset_complete'),
->>>>>>> 8831daed8dccee3c2719cc1795fc839df3baa312
 
 ]
